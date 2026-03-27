968fe332be5ea70519091f67c8645676b1626b10
Niccolò <niccolo.sorrenti@gmail.com>
2026-03-27 10:21:05 +0100

    connessione al DB, fatta

--- src/main/java/niccolosorrenti/Application.java
+++ src/main/java/niccolosorrenti/Application.java
@@ -1,6 +1,11 @@
 package niccolosorrenti;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("catalogobibliograficopu");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
